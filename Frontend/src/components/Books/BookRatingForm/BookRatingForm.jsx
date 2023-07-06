7f56b59ead591f9e9362d9f59791a98ad510e017
Erik-42 <mesen.erik@gmail.com>
2023-07-06 14:58:35 +0200

    nettoyageCodeEtCommentaires2

--- Frontend/src/components/Books/BookRatingForm/BookRatingForm.jsx
+++ Frontend/src/components/Books/BookRatingForm/BookRatingForm.jsx
@@ -31,7 +31,6 @@ function BookRatingForm({
       navigate(APP_ROUTES.SIGN_IN);
     }
     const update = await rateBook(id, userId, rating);
-    console.log(update);
     if (update) {
       // eslint-disable-next-line no-underscore-dangle
       setBook({ ...update, id: update._id });
